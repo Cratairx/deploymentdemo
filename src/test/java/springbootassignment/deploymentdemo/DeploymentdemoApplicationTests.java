99defef4302d57d2a7b82d8a79619c714c2aec75
Cratairx <daniel.inserte@gmail.com>
2026-09-03 09:45:05 +0200

    första

--- src/test/java/springbootassignment/deploymentdemo/DeploymentdemoApplicationTests.java
+++ src/test/java/springbootassignment/deploymentdemo/DeploymentdemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DeploymentdemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
