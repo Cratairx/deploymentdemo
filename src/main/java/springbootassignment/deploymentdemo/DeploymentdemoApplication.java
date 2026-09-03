604f9871207b7bc8fe4e54883101025b8e8adb41
Cratairx <daniel.inserte@gmail.com>
2026-09-03 09:44:33 +0200

    Initial commit

---
+++ src/main/java/springbootassignment/deploymentdemo/DeploymentdemoApplication.java
@@ -0,0 +1,13 @@
+package springbootassignment.deploymentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbootassignment/deploymentdemo/DeploymentdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootassignment.deploymentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
